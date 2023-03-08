bf053802d7aa8ae121225d6f9c931c6db4a6692d
Yehwan Baek <baekokok98@gmail.com>
2023-03-08 23:00:25 +1100

    added code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity = "go to the office") { 
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(highlight = "*") {
+    let result = function(comment = "special"){
+        return `You are ${highlight}${comment}${highlight}!`;
+    }
+    return result;
+}
